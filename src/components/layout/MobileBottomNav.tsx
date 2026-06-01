9ca523a1dc124df6e20e95610c1e00ec58fa8c93
Ridwan Elly <ridwanelly123@gmail.com>
2026-06-01 13:49:00 +0900

    Mobile UX: bottom nav, sidebar label fixes, header z-index

--- src/components/layout/MobileBottomNav.tsx
+++ src/components/layout/MobileBottomNav.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import { HomeIcon, CalendarIcon, ClipboardIcon, ChartBarIcon, UserIcon } from "@/icons";
 
 const MobileBottomNav: React.FC = () => {
   return (
