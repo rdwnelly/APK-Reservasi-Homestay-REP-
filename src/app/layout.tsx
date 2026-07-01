241e564b734e6a4503be8355b6e15f2d04411c6a
Ridwan Elly <ridwanelly123@gmail.com>
2026-07-01 17:27:55 +0900

    Update PWA manifest and apple-touch-icon for new app logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#101828" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <link rel="apple-touch-icon" href="/images/logo/logorep.jpg" />
+        <link rel="apple-touch-icon" href="/images/logo/app-icon.png" />
       </head>
       <body className={`${inter.variable} ${lora.variable} font-sans`}>
         <ThemeProvider>
